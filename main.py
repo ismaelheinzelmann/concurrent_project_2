9236d14e0906664ae228c46023a54d54681e9d87
Ismael Heinzelmann <67344134+ismaelheinzelmann@users.noreply.github.com>
2023-06-26 03:28:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,105 +1,151 @@
+from math import ceil
+
 from classes.SudokuParser import SudokuParser
 from multiprocessing import Process, Queue
 from threading import Thread, Lock, Semaphore
-from funcs import (
-    verify_line,
-    verify_column,
-    verify_subgrid,
-    clear_errors,
-    print_results,
-)
+from funcs import verify_line, verify_column, verify_subgrid
 import sys
 import time
 
 
-def thread_process(i, threads_queue, errors, tables, lock, sem):
+def clear_errors(errors):
+    for table in errors:
+        for thread in table:
+            thread.clear()
+
+
+def thread_process(
+    id,
+    id_thread,
+    start,
+    threads_queue,
+    errors,
+    tables,
+    tables_n,
+    semaphores,
+    lock,
+    already_lock,
+    already,
+):
     while True:
         request = threads_queue.get()
         if request == (-1, -1, -1):
-            sem.release()
-            return
+            break
         (table, req, index) = request
+        with already_lock[table]:
+            if not already[table]:
+                print(
+                    f"Processo {id + 1}: resolvendo quebra-cabeças {int((start/10)+table) + 1}"
+                )
+                already[table] = True
         response = ""
         valid = bool()
         if req == "line":
             response = f"L{index}"
-            valid = verify_line(tables[table - 1].get_line(int(index)))
+            valid = verify_line(tables[table].get_line(int(index)))
         elif req == "column":
             response = f"C{index}"
-            valid = verify_column(tables[table - 1].get_column(int(index)))
+            valid = verify_column(tables[table].get_column(int(index)))
         elif req == "subgrid":
             response = f"R{index}"
-            valid = verify_subgrid(tables[table - 1].get_block(int(index)))
+            valid = verify_subgrid(tables[table].get_block(int(index)))
         if not valid:
             with lock:
-                errors[i].append(response)
-        sem.release()
+                errors[table][id_thread].append(response)
+        semaphores[table].release()
 
 
-def process_worker(id, requests_queue, threads_n, tables):
-    errors = [[] for _ in range(threads_n)]
+def process_worker(id, start, filename, tables_n, threads_n):
+    tables = SudokuParser(filename, start, start + tables_n * 10).getTables()
+    errors = [[[] for _ in range(threads_n)] for _ in range(len(tables))]
+    already = [False for _ in range(tables_n)]
+    already_lock = [Lock() for _ in range(tables_n)]
+    for i in range(1, tables_n):
+        already_lock[i].acquire()
     lock = Lock()
     threads_queue = Queue()
-    sem_done = Semaphore(0)
+    for table in range(len(tables)):
+        for line in range(1, 10):
+            threads_queue.put((table, "line", line))
+        for column in range(1, 10):
+            threads_queue.put((table, "column", column))
+        for subgrid in range(1, 10):
+            threads_queue.put((table, "subgrid", subgrid))
+    for _ in range(threads_n):
+        threads_queue.put((-1, -1, -1))
+
+    semaphores = [Semaphore(0) for _ in range(tables_n)]
     threads = [
         Thread(
             target=thread_process,
-            args=(i, threads_queue, errors, tables, lock, sem_done),
+            args=(
+                id,
+                i,
+                start,
+                threads_queue,
+                errors,
+                tables,
+                len(tables),
+                semaphores,
+                lock,
+                already_lock,
+                already,
+            ),
         )
         for i in range(threads_n)
     ]
     for thread in threads:
         thread.start()
-    working_job = None
-    counter = 0
-    while True:
-        request = requests_queue.get()
-        if request == (-1, -1, -1):
-            for _ in range(threads_n):
-                threads_queue.put((-1, -1, -1))
-            for _ in range(counter):
-                sem_done.acquire()
-                counter -= 1
-            print_results(errors, id)
-            for thread in threads:
-                thread.join()
-            return
-        if working_job is None:
-            working_job = request[0]
-            print(f"Processo {id + 1}: resolvendo quebra-cabeças {working_job}")
-        if working_job != request[0]:
-            for _ in range(counter):
-                sem_done.acquire()
-                counter -= 1
-            print_results(errors, id)
+    for table in range(len(tables)):
+        for _ in range(27):
+            semaphores[table].acquire()
+        total_errors = 0
+        for table_e in errors[table]:
+            total_errors += len(table_e)
+        if total_errors == 0:
+            print(f"Processo {id + 1}: 0 erros encontrados.")
+        else:
+            print(f"Processo {id + 1}: {total_errors} erros encontrados (", end="")
+            final_append = [
+                f"T{i + 1}: {', '.join(e)}"
+                for i, e in enumerate(errors[table])
+                if len(e) > 0
+            ]
+            print("; ".join(final_append) + ")")
+        if table != tables_n - 1:
             clear_errors(errors)
-            working_job = request[0]
-            print(f"Processo {id + 1}: resolvendo quebra-cabeças {working_job}")
-            counter = 0
-        threads_queue.put(request)
-        counter += 1
+            already_lock[table + 1].release()
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
-    TABLES = SudokuParser(sys.argv[1]).getTables()
+    start_time = time.time()
+    N_LINES = 0
+    with open(sys.argv[1], "r") as file:
+        N_LINES = len(file.readlines())
     NUM_PROCESS = int(sys.argv[2])
     THREADS_PER_PROCESS = int(sys.argv[3])
-    N_TABLES = len(TABLES)
+    N_TABLES = ceil(N_LINES / 10)
+    if NUM_PROCESS > N_TABLES:
+        NUM_PROCESS = N_TABLES
+    dist = [N_TABLES // NUM_PROCESS for _ in range(NUM_PROCESS)]
+    next_start = 0
+    for i in range(N_TABLES % NUM_PROCESS):
+        dist[i] += 1
     requests = Queue()
-    processes = [
-        Process(target=process_worker, args=(i, requests, THREADS_PER_PROCESS, TABLES))
-        for i in range(NUM_PROCESS)
-    ]
+    processes = []
+    for i in range(NUM_PROCESS):
+        processes.append(
+            Process(
+                target=process_worker,
+                args=(i, next_start, sys.argv[1], dist[i], THREADS_PER_PROCESS),
+            )
+        )
+        next_start += dist[i] * 10
     for p in processes:
         p.start()
-    for table in range(1, N_TABLES + 1):
-        for line in range(1, 10):
-            requests.put((table, "line", line))
-        for column in range(1, 10):
-            requests.put((table, "column", column))
-        for subgrid in range(1, 10):
-            requests.put((table, "subgrid", subgrid))
-    for _ in range(NUM_PROCESS):
-        requests.put((-1, -1, -1))
     for p in processes:
         p.join()
+    print(f"Tempo de execução: {time.time() - start_time} segundos.")
